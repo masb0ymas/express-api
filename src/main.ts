928f9621be7db61e5a9ba34c5a76eaf5fa35b746
masb0ymas <n.fajri@outlook.com>
2025-03-31 14:56:55 +0700

    fix: update code

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,11 @@ import http from 'http'
 import { App } from './config/app'
 import { env } from './config/env'
 import { httpHandle } from './lib/http/handle'
-import { initDatabase } from './app/database/connection'
 
 function bootstrap() {
   const port = env.APP_PORT
   const app = new App().create
   const server = http.createServer(app)
-
-  initDatabase()
 
   // http handle
   const { onError, onListening } = httpHandle(server, port)
